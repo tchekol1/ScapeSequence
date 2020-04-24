2a8791684d9c18aa7b564f596e71b76b0128af56
tchekkol1 <tchekol1@umbc.edu>
2020-04-23 22:21:16 -0400

    first commit

---
+++ src/ScapeSequence.java
@@ -0,0 +1,18 @@
+public class ScapeSequence {
+    public static void main(String[] args) {
+        for (int i = 1; i < 8; i++) {
+            for (int j = 1; j < 8; j++) {
+                if(i==1||i==7){
+
+                System.out.print("*");}
+                else if(j==1||j==7)
+                    System.out.print("*"+ " ");
+                else
+                    System.out.print(" ");
+            }
+
+            System.out.println();
+        }
+    }
+}
+
